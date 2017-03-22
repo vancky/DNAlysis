06e737bca19cbe007e27e2df2d0f51c975f0cbe8
DASTUD\jvanderauweraert <j.f.vanderauweraert@student.tudelft.nl>
2017-03-22 17:07:56 +0100

    No real update

--- HelicaseTracking/Functions/HelicaseFitter.m
+++ HelicaseTracking/Functions/HelicaseFitter.m
@@ -1,6 +1,6 @@
 function [ output_args ] = HelicaseFitter( image )
     %HELICASETRACKER Fits a gaussian for the different helicases
-
+    % We can use the CLEAN algorithm for this purpose,just with a higher threshold :)
 
 
 end
