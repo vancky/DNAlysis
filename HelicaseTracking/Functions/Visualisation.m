e05dede61ea85f807c8ed94215af0393c9870f39
DASTUD\jvanderauweraert <j.f.vanderauweraert@student.tudelft.nl>
2017-03-28 15:32:15 +0200

    Back to the old setup (one scaleValue not 100 ;))

--- HelicaseTracking/Functions/Visualisation.m
+++ HelicaseTracking/Functions/Visualisation.m
@@ -13,28 +13,18 @@ function [ output_args ] = Visualisation( config, simulateImages , helicaseFitte
      xlabel('Frame')
      ylabel('Error (nm)')
 
-     
-     figure
-     hold on
-     plot(5:40, 1e9*avg);
-     plot(5:40, 1e9*maximum);
-     hold off
-     title('Error due to limited photon count')
-     legend('average error','maximum error')
-     xlabel('Maximum photon count')
-     ylabel('Error (nm)')
-%     for i=1:5
-%        ydot=helicaseFitter.fitLocation(10*i,1);
-%        xdot=helicaseFitter.fitLocation(10*i,2);
-%        
-%        figure;
-%        
-%        imshow(simulateImages.simulateHelicases.image{10*i},[])
-%        hold on
-%        
-%        plot(xdot, ydot, 'r.' , 'MarkerSize' , 5);
-%        hold off
-%              
-%       end
+    for i=1:5
+       ydot=helicaseFitter.fitLocation(10*i,1);
+       xdot=helicaseFitter.fitLocation(10*i,2);
+       
+       figure;
+       
+       imshow(simulateImages.simulateHelicases.image{10*i},[])
+       hold on
+       
+       plot(xdot, ydot, 'r.' , 'MarkerSize' , 5);
+       hold off
+             
+      end
 end
 
--- HelicaseTracking/Main.m
+++ HelicaseTracking/Main.m
@@ -3,32 +3,25 @@ clear all; clc; close all;
 config=struct();
 config=Config(config);
 
-%%
-numSimulations=36;
+%% Simulate a set of images
 
-for i=1:numSimulations
-    %% Simulate a set of images
-    fprintf('Running simulations %d/%d.\n',i , numSimulations)
-    config.scaleValue=4+i;
-    simulateImages=SimulateImages(config);
-    
+simulateImages=SimulateImages(config);
 
-    %% Track the Helicases
 
-        % Tracks helicases along different images
-        % 1. For each individual image fit the helicases as Gaussians
-        % 3. Make a big matrix with position of each individual helicase over time
-        % 4. Obtain valuable information, such as velocity
+%% Track the Helicases
 
-    helicaseFitter=HelicaseFitter(config, simulateImages);
+    % Tracks helicases along different images
+    % 1. For each individual image fit the helicases as Gaussians
+    % 3. Make a big matrix with position of each individual helicase over time
+    % 4. Obtain valuable information, such as velocity
 
-    %% Analysis
+helicaseFitter=HelicaseFitter(config, simulateImages);
 
-    analysis{i}=Analysis(config, simulateImages, helicaseFitter);
-    
-    avg(i)=mean(analysis{i}.errorMeters(:,3));
-    maximum(i)=max(analysis{i}.errorMeters(:,3));
-end
+%% Analysis
+
+analysis=Analysis(config, simulateImages, helicaseFitter);
+
+
  
 %% Visualisation
 
