9425691739b32049f402b52be9b387b4ca9ee892
DASTUD\jvanderauweraert <j.f.vanderauweraert@student.tudelft.nl>
2017-04-18 14:41:49 +0200

    Mini commit

--- HelicaseTracking/Functions/BeamshapeCorrection.m
+++ HelicaseTracking/Functions/BeamshapeCorrection.m
@@ -1,4 +1,4 @@
-function [ output ] = BeamshapeCorrection( config , beamshape, images )
+function [ output ] = BeamshapeCorrection( beamshape, images )
     % Beamshape Correction - Corrects for the beamshape profile
     % Please input the images as a 3D matrix, i.e. images(:,:,N) where N is
     % the total number of images
@@ -6,7 +6,7 @@ function [ output ] = BeamshapeCorrection( config , beamshape, images )
     % Multiplies the normalized beamshape with the images
     
     numImages=size(images,3);
-    output=zeros(config.pixels,config.pixels,numImages, class(images));
+    output=zeros(size(images));
     
     for i=1:numImages
        output(:,:,i)=double(images(:,:,i)).*beamshape;
@@ -14,7 +14,6 @@ function [ output ] = BeamshapeCorrection( config , beamshape, images )
        subplot(1,2,1); imshow(images(:,:,i),[]); title('Input Image');
        subplot(1,2,2); imshow(output(:,:,i),[]); title('Beamshape Corrected');
     end
-    
     
     fprintf('Beamshape Corrected.\n')
 end
