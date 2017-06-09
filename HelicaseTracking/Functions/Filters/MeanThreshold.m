6e9572dc84a5b50958f3440ee68f6ffe1f70d3ea
DASTUD\jvanderauweraert <j.f.vanderauweraert@student.tudelft.nl>
2017-06-09 12:48:13 +0200

    Created a mean and median global threshold function

---
+++ HelicaseTracking/Functions/Filters/MeanThreshold.m
@@ -0,0 +1,9 @@
+function [ outputImage ] = MeanThreshold( inputImage )
+    % Global Mean Filter - Uses the global mean of an image as a
+    % threshold, values below the mean are set to 0.
+    
+    threshold = mean( inputImage(:) );
+    outputImage = inputImage;
+    outputImage( inputImage <threshold ) = 0;
+end
+
--- HelicaseTracking/Functions/HelicaseFitting/WatershedImage.m
+++ HelicaseTracking/Functions/HelicaseFitting/WatershedImage.m
@@ -2,7 +2,8 @@ function [ outputImage ] = WatershedImage( inputImage )
     % HelicaseWatershed - Give as input an image, this function will find
     % the watershed lines distinguishing different local maxima in the
     % picture and will set these lines to 0 in the original image
-
+    
+    inputImage = MeanThreshold( inputImage );
     invertImage = abs(inputImage - max(inputImage(:)));
     watershedImage = watershed(invertImage);
     binary = watershedImage>0;
@@ -11,8 +12,8 @@ function [ outputImage ] = WatershedImage( inputImage )
 %     The lines below can be uncommented to visualise if the WatershedImage
 %     function is performing as you expect. 
 
-%     figure;
-%     subplot(1,2,1); imshow( inputImage , [0 50]); title('Helicase Image'); colorbar
-%     subplot(1,2,2); imshow( outputImage , [0 50]); title('Watershed Helicase Image'); colorbar
+    figure;
+    subplot(1,2,1); imshow( inputImage , [0 50]); title('Helicase Image'); colorbar
+    subplot(1,2,2); imshow( outputImage , [0 50]); title('Watershed Helicase Image'); colorbar
 end
 
