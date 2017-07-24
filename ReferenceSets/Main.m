76beecd9d412f784147f8f2f7173650a2ae02c84
DASTUD\jvanderauweraert <j.f.vanderauweraert@student.tudelft.nl>
2017-07-24 14:34:03 +0200

    We can now manually clean the images, time to select the correct ROIs

--- ReferenceSets/Main.m
+++ ReferenceSets/Main.m
@@ -5,13 +5,15 @@ config=Config(config);
 
 %% Create and save the reference sets
 
-
+%Reference Set 1
 referenceSet1 = CreateReferenceSet1( config );
 save( strcat(config.referenceSetSaveCd, 'ReferenceSet1.mat') , 'referenceSet1');
-
+% % Reference Set 2
 % referenceSet2 = CreateReferenceSet2( config );
-% SaveReferenceSet2( referenceSet2 )
+% save( strcat(config.referenceSetSaveCd, 'ReferenceSet1.mat') , 'referenceSet2');
+% % Reference Set 3
 % referenceSet3 = CreateReferenceSet3( config );
-% SaveReferenceSet3( referenceSet3 )
+% save( strcat(config.referenceSetSaveCd, 'ReferenceSet1.mat') , 'referenceSet3');
+% % Reference Set4
 % referenceSet4 = CreateReferenceSet4( config );
-% SaveReferenceSet4( referenceSet4 )
+% save( strcat(config.referenceSetSaveCd, 'ReferenceSet1.mat') , 'referenceSet4');
