5952450437f57d728b2faa5ddc4072deb211059d
DASTUD\jvanderauweraert <j.f.vanderauweraert@student.tudelft.nl>
2017-08-07 15:54:33 +0200

    Reference Set 3 is done!

--- ReferenceSets/Functions/ReferenceSet3/CreateHelicaseImage3.m
+++ ReferenceSets/Functions/ReferenceSet3/CreateHelicaseImage3.m
@@ -3,31 +3,31 @@ function [ output ] = CreateHelicaseImage3( config )
     % Creates the static helicase image
 
     % Specify the locations of the helicases to be simulated [x,y]
-    locations(1,:) = [ 10, 10];
-    locations(2,:) = [ 30, 30];
-%     locations(3,:) = [ , ];
-%     locations(4,:) = [ , ];
-%     locations(5,:) = [ , ];
-%     locations(6,:) = [ , ];
-%     locations(7,:) = [ , ];
-%     locations(8,:) = [ , ];
-%     locations(9,:) = [ , ];
-%     locations(10,:) = [ , ];
-%     locations(11,:) = [ , ];
-%     locations(12,:) = [ , ];
-%     locations(13,:) = [ , ];
-%     locations(14,:) = [ , ];
-%     locations(15,:) = [ , ];
-%     locations(16,:) = [ , ];
-%     locations(17,:) = [ , ];
-%     locations(18,:) = [ , ];
-%     locations(19,:) = [ , ];
-%     locations(20,:) = [ , ];
-%     locations(21,:) = [ , ];
-%     locations(22,:) = [ , ];
-%     locations(23,:) = [ , ];
-%     locations(24,:) = [ , ];
-%     locations(25,:) = [ , ]; 
+    locations(1,:) = [ 32, 20];
+    locations(2,:) = [ 56, 20];
+    locations(3,:) = [ 63, 100];
+    locations(4,:) = [ 97, 100];
+    locations(5,:) = [ 126, 180];
+    locations(6,:) = [ 154, 180];
+    locations(7,:) = [ 146, 260];
+    locations(8,:) = [ 189, 260];
+    locations(9,:) = [ 203, 340];
+    locations(10,:) = [ 214, 340];
+    locations(11,:) = [ 50, 50];
+    locations(12,:) = [ 20, 70];
+    locations(13,:) = [ 164, 100];
+    locations(14,:) = [ 62, 130];
+    locations(15,:) = [ 228, 150];
+    locations(16,:) = [ 52, 160];
+    locations(17,:) = [ 143, 160];
+    locations(18,:) = [ 255, 190];
+    locations(19,:) = [ 128, 190];
+    locations(20,:) = [ 24, 220];
+    locations(21,:) = [ 133, 270];
+    locations(22,:) = [ 75, 320];
+    locations(23,:) = [ 100, 360];
+    locations(24,:) = [ 148, 410];
+    locations(25,:) = [ 162, 450]; 
     
     % Simulate the helicases 
     helicaseImage = zeros( config.imageSize);
