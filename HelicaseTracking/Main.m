7a6c6fdfcf621e1da9326c4b6515640c24d8f314
DASTUD\jvanderauweraert <j.f.vanderauweraert@student.tudelft.nl>
2017-08-22 10:25:58 +0200

    tracking matchhelicases

--- HelicaseTracking/Main.m
+++ HelicaseTracking/Main.m
@@ -49,4 +49,4 @@ analysis = Analysis( config, preProcess);
 %% Post Processing
 
 fprintf('This section performs the post processing.\n')
-postProcess = PostProcess( config , analysis.matchDnaHelicase , analysis.helicaseIntensity , analysis.spotFinder);
+%postProcess = PostProcess( config , analysis.matchDnaHelicase , analysis.helicaseIntensity , analysis.spotFinder);
